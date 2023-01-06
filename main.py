e606726612b7d6f9726d7b23e34fc5ff32856107
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2023-01-06 20:19:08 +0000

    listo

--- main.py
+++ main.py
@@ -15,88 +15,89 @@ mongo = os.environ['mongo']
 otra = str(mongo)
 client = MongoClient(otra)
 cors = CORS(app, resources={r"/user/*": {"origins": "*"}})
-app.config["UPLOAD_FOLDER"] ="archivos"
-ALLOWED_EXTENSIONS =set(['jpg','png','jpeg','gif'])
+app.config["UPLOAD_FOLDER"] = "archivos"
+ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg', 'gif','pdf','txt','doc','docx'])
 
-def extesiones():
+
+def extesiones(file):
   file = file.split('.')
   if file[1] in ALLOWED_EXTENSIONS:
     return True
   return False
 
-@app.route('/', methods=['POST'])
+
+@app.route('/user/crear', methods=['POST'])
 def create_user():
-    nombre = request.json['nombre']
-    numero = request.json['numero']
-    apellidos = request.json['apellidos']
-    enfermedad = request.json['enfermedad']
-    password = request.json['password']
-    email = request.json['email']
+  nombre = request.json['nombre']
+  numero = request.json['numero']
+  apellidos = request.json['apellidos']
+  enfermedad = request.json['enfermedad']
+  password = request.json['password']
+  email = request.json['email']
 
-    if nombre and numero and password and email:
-        cifrado = generate_password_hash(password)
-        id = client.db.users.insert_one({
-            'nombre': nombre,
-            'numero': numero,
-            'apellidos': apellidos,
-            'enfermeda': enfermedad,
-            'password': cifrado,
-            'email': email
-        })
-        response = {
-            'id': str(id),
-            'nombre': nombre,
-            'numero': numero,
-            'email': email,
-            'apellidos': apellidos,
-            'enfermeda': enfermedad,
-            'password': cifrado,
-        }
-        return response
-    else:
-        {'message': 'No recibido'}
+  if nombre and numero and password and email:
+    cifrado = generate_password_hash(password)
+    id = client.db.users.insert_one({
+      'nombre': nombre,
+      'numero': numero,
+      'apellidos': apellidos,
+      'enfermeda': enfermedad,
+      'password': cifrado,
+      'email': email
+    })
+    response = {
+      'id': str(id),
+      'nombre': nombre,
+      'numero': numero,
+      'email': email,
+      'apellidos': apellidos,
+      'enfermeda': enfermedad,
+      'password': cifrado,
+    }
+    return response
+  else:
+    {'message': 'No recibido'}
 
-    return {'message': 'recibido'}
+  return {'message': 'recibido'}
 
 
 @app.route('/user', methods=['GET'])
 def encontra():
-    users = client.db.users.find()
-    response = json_util.dumps(users)
-    return Response(response, mimetype='application/json')
+  users = client.db.users.find()
+  response = json_util.dumps(users)
+  return Response(response, mimetype='application/json')
 
 
 @app.route('/user/<id>', methods=['GET'])
 def consulta(id):
-    #Para consulta los paccientes
-    user = client.db.users.find_one({'_id:ObjectId(id)'})
+  #Para consulta los paccientes
+  user = client.db.users.find_one({'_id:ObjectId(id)'})
+  if(len(user)>0):
     response = json_util.dumps(user)
     return response
+  return jsonify({"message":"no encontrado"})
 
 
 @app.route('/user/archivos', methods=['POST'])
 def guardaArchivo():
-  file= request.file["uploadFile"]
+  file = request.file["uploadFile"]
   filename = secure_filename(file.filename)
   if file and extesiones(filename):
-    print("permitido")
-    file.save(os.path.join(app.config["UPLOAD_FOLDER"],filename))
-  
-  
-  
-  return "recibido"
-  
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    return jsonify({"message":"Subido correctamente"})
+  return jsonify({"message":"No subido"})
+
 
 @app.errorhandler(404)
 def not_encontrado(error=None):
-    message = jsonify({
-        'message': 'Pagina no encontrada' + request.url,
-        'status': 404
-    })
-    message.status_code = 404
-    return message
+  message = jsonify({
+    'message': 'Pagina no encontrada' + request.url,
+    'status': 404
+  })
+  message.status_code = 404
+  return message
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=81)
+  app.run(debug=True, host='0.0.0.0', port=81)
 0
